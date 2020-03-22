ccf7ab4379bd34df0e3df172b29994eabfc5f029
ChenJR <827040662@qq.com>
2020-03-22 16:53:01 +0800

    1560. 面试题56 - I. 数组中数字出现的次数

---
+++ maxValue.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * 1560. 面试题56 - I. 数组中数字出现的次数
+ * Class MaxValueSolution
+ * https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/
+ */
+class MaxValueSolution
+{
+
+    private $grid;
+    private $max_value = PHP_INT_MIN;
+
+    /**
+     * DP
+     * @param Integer[][] $grid
+     * @return Integer
+     */
+    function maxValue($grid)
+    {
+//        $this->grid = $grid;
+//        $this->getValue(0, 0, 0);
+//        return $this->max_value;
+
+        $m = count($grid)-1;
+        $n = count($grid[0])-1;
+        for ($i = 0; $i <= $m; $i++) {
+            for ($k = 0; $k <= $n; $k++) {
+                $grid[$i][$k] = max($grid[$i][$k-1] ?: 0, isset($grid[$i-1]) ? $grid[$i-1][$k] : 0) + $grid[$i][$k];
+            }
+        }
+        return $grid[$m][$n];
+    }
+
+    /**
+     * 递归 超时解
+     * @param $row
+     * @param $column
+     * @param $value
+     */
+    function getValue($row, $column, $value)
+    {
+        $value += $this->grid[$row][$column];
+        if (isset($this->grid[$row + 1][$column])) {
+            //可以向下走
+            $this->getValue($row + 1, $column, $value);
+        }
+        if (isset($this->grid[$row][$column + 1])) {
+            $this->getValue($row, $column + 1, $value);
+        }
+        if (!isset($this->grid[$row][$column + 1]) && !isset($this->grid[$row + 1][$column])) {
+            $this->max_value = max($this->max_value, $value);
+        }
+        return;
+    }
+}
--- singleNumber.php
+++ singleNumber.php
@@ -2,6 +2,10 @@
 
 /**
  * 1552. 面试题56 - II. 数组中数字出现的次数 II
+ * 1560. 面试题56 - I. 数组中数字出现的次数
+ *
+ * 理想做法为位运算
+ *
  * Class SingleNumberSolution
  * https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/
  */
@@ -27,5 +31,26 @@ class SingleNumberSolution
                 return $nums[$i];
             }
         }
+    }
+
+    /**
+     * 1560解法 排序后解 非理想做法
+     * @param $nums
+     * @return array
+     */
+    function singleNumbers2($nums)
+    {
+        $res = [];
+        sort($nums);
+        $i = 0;
+        while ($i <= count($nums) - 1) {
+            if ($nums[$i] == $nums[$i + 1]) {
+                $i += 2;
+            } else {
+                $res[] = $nums[$i];
+                $i++;
+            }
+        }
+        return $res;
     }
 }
