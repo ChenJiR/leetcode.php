8b3453b24402961d115f42dc78274f9f94c9c8cf
ChenJR <827040662@qq.com>
2020-03-25 09:13:58 +0800

    1536. 面试题32 - I. 从上到下打印二叉树 1537. 面试题32 - II. 从上到下打印二叉树 II

--- levelOrder.php
+++ levelOrder.php
@@ -1,33 +1,61 @@
 <?php
 
 /**
- * 1524. 面试题32 - II. 从上到下打印二叉树 II
+ * 1536. 面试题32 - I. 从上到下打印二叉树
+ * 1537. 面试题32 - II. 从上到下打印二叉树 II
  * Class LevelOrderSolution
+ * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof/
  * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
  */
-class LevelOrderSolution {
+class LevelOrderSolution
+{
 
     /**
+     * 1536. 面试题32 - I. 从上到下打印二叉树
+     * BFS
      * @param TreeNode $root
      * @return Integer[][]
      */
-    function levelOrder($root) {
-        if($root == null){
+    function levelOrder($root)
+    {
+        $res = [];
+        $list = [$root];
+        while (!empty($list)) {
+            $tree = array_shift($list);
+            if ($tree) {
+                $res[] = $tree->val;
+                $tree->left && $list[] = $tree->left;
+                $tree->right && $list[] = $tree->right;
+            }
+        }
+        return $res;
+    }
+
+    /**
+     * 1537. 面试题32 - II. 从上到下打印二叉树 II
+     * DFS
+     * @param TreeNode $root
+     * @return Integer[][]
+     */
+    function levelOrder2($root)
+    {
+        if ($root == null) {
             return [];
         }
         $result = [];
-        $this->generateLevel($root,0,$result);
+        $this->generateLevel($root, 0, $result);
         return $result;
     }
 
-    private function generateLevel(TreeNode $root,$rootLevel,&$result){
-        if($root != null){
+    private function generateLevel(TreeNode $root, $rootLevel, &$result)
+    {
+        if ($root != null) {
             isset($result[$rootLevel]) ? $result[$rootLevel][] = $root->val : $result[$rootLevel] = [$root->val];
-            if($root->left != null){
-                $this->generateLevel($root->left,$rootLevel+1,$result);
+            if ($root->left != null) {
+                $this->generateLevel($root->left, $rootLevel + 1, $result);
             }
-            if($root->right != null){
-                $this->generateLevel($root->right,$rootLevel+1,$result);
+            if ($root->right != null) {
+                $this->generateLevel($root->right, $rootLevel + 1, $result);
             }
         }
     }
