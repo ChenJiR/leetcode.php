bc63dcdac21e779e60d67bff835743bcc735627b
ChenJR <827040662@qq.com>
2020-04-02 21:06:09 +0800

    88. 合并两个有序数组 1439. 面试题 10.01. 合并排序的数组

--- merge.php
+++ merge.php
@@ -1,11 +1,13 @@
 <?php
 
 /**
+ * 88. 合并两个有序数组
  * 1439. 面试题 10.01. 合并排序的数组
  * Class MergeSolution
  * https://leetcode-cn.com/problems/sorted-merge-lcci/
  */
-class MergeSolution {
+class MergeSolution
+{
 
     /**
      * @param Integer[] $A
@@ -14,10 +16,34 @@ class MergeSolution {
      * @param Integer $n
      * @return NULL
      */
-    function merge(&$A, $m, $B, $n) {
-        foreach($B as $item){
+    function merge(&$A, $m, $B, $n)
+    {
+        foreach ($B as $item) {
             $A[$m++] = $item;
         }
         sort($A);
+    }
+
+    /**
+     * 双指针
+     * @param $nums1
+     * @param Integer $m
+     * @param $nums2
+     * @param Integer $n
+     * @return NULL
+     */
+    function merge2(&$nums1, $m, $nums2, $n)
+    {
+        $tmp = [];
+        $a = $b = 0;
+        while ($a < $m || !empty($nums2)) {
+            if ($a < $m && $nums1[$a] <= (isset($nums2[0]) ? $nums2[0] : PHP_INT_MAX)) {
+                $tmp[] = $nums1[$a];
+                $a++;
+            } else {
+                $tmp[] = array_shift($nums2);
+            }
+        }
+        $nums1 = array_merge($tmp, $nums2);
     }
 }
