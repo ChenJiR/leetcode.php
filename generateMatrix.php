10092d8a5bd8f0ff7b240bd51c1d4d6d1bbb6963
ChenJR <827040662@qq.com>
2020-04-10 11:30:12 +0800

    59. 螺旋矩阵 II

---
+++ generateMatrix.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * 59. 螺旋矩阵 II
+ * Class GenerateMatrixSolution
+ * https://leetcode-cn.com/problems/spiral-matrix-ii/
+ */
+class GenerateMatrixSolution
+{
+
+    /**
+     * @param Integer $n
+     * @return Integer[][]
+     */
+    function generateMatrix($n)
+    {
+        $top = $left = 0;
+        $right = $buttom = $n - 1;
+        $res = [];
+        for ($i = 1; $i <= $n; $i++) {
+            $res[] = range(1, $n);
+        }
+        $index = 1;
+        $max = $n * $n;
+        while ($index <= $max) {
+            for ($i = $left; $i <= $right; $i++) $res[$top][$i] = $index++;
+            if ($index > $max) return $res;
+            $top++;
+            for ($i = $top; $i <= $buttom; $i++) $res[$i][$right] = $index++;
+            if ($index > $max) return $res;
+            $right--;
+            for ($i = $right; $i >= $left; $i--) $res[$buttom][$i] = $index++;
+            if ($index > $max) break;
+            $buttom--;
+            for ($i = $buttom; $i >= $top; $i--) $res[$i][$left] = $index++;
+            if ($index > $max) break;
+            $left++;
+        }
+        return $res;
+    }
+}
