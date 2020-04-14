b6c8eadeab773749bff88c38ce63e6c0b2eefce0
ChenJR <827040662@qq.com>
2020-04-14 08:29:48 +0800

    单向链表

--- ListNode.php
+++ ListNode.php
@@ -11,5 +11,26 @@ class ListNode
 {
     public $val = 0;
     public $next = null;
-    function __construct($val) { $this->val = $val; }
+
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+
+    /**
+     * 数组转换为单向链表
+     * @param array $ary
+     * @return ListNode
+     */
+    static function arrayToList(array $ary)
+    {
+        $cur = new static(null);
+        $res = $cur;
+        while (!empty($ary)) {
+            $node = new static(array_shift($ary));
+            $cur->next = $node;
+            $cur = $cur->next;
+        }
+        return $res->next;
+    }
 }
