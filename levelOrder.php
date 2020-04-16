ef78c3da1bda1782d591f308f2c6672b063cc9ca
ChenJR <827040662@qq.com>
2020-04-16 21:46:42 +0800

    102. 二叉树的层次遍历 429. N叉树的层序遍历 1536. 面试题32 - I. 从上到下打印二叉树 1537. 面试题32 - II. 从上到下打印二叉树 II

--- levelOrder.php
+++ levelOrder.php
@@ -3,15 +3,43 @@
 /**
  * 102. 二叉树的层次遍历
  * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
- * 
+ * 429. N叉树的层序遍历
+ * https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
  * 1536. 面试题32 - I. 从上到下打印二叉树
- * 1537. 面试题32 - II. 从上到下打印二叉树 II
- * Class LevelOrderSolution
  * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof/
+ * 1537. 面试题32 - II. 从上到下打印二叉树 II
  * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+ *
+ * Class LevelOrderSolution
  */
 class LevelOrderSolution
 {
+
+    /**
+     * 429. N叉树的层序遍历
+     * BFS
+     * @param NTreeNode $root
+     * @return Integer[][]
+     */
+    function levelOrder_429($root)
+    {
+        if (!$root) return [];
+        $ary = [$root];
+        $res = [];
+        while (!empty($ary)) {
+            $floor_val = [];
+            $floor = [];
+            foreach ($ary as $item) {
+                $floor_val[] = $item->val;
+                foreach ($item->children as $node) {
+                    $floor[] = $node;
+                }
+            }
+            $res[] = $floor_val;
+            $ary = $floor;
+        }
+        return $res;
+    }
 
     /**
      * 1536. 面试题32 - I. 从上到下打印二叉树
@@ -19,7 +47,7 @@ class LevelOrderSolution
      * @param TreeNode $root
      * @return Integer[][]
      */
-    function levelOrder($root)
+    function levelOrder_1536($root)
     {
         $res = [];
         $list = [$root];
@@ -35,12 +63,12 @@ class LevelOrderSolution
     }
 
     /**
-     * 1537. 面试题32 - II. 从上到下打印二叉树 II
+     * 102 1537. 面试题32 - II. 从上到下打印二叉树 II
      * DFS
      * @param TreeNode $root
      * @return Integer[][]
      */
-    function levelOrder2($root)
+    function levelOrder_102_1537($root)
     {
         if ($root == null) {
             return [];
