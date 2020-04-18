97c8bf63063de22fe9bf3ddc588354d6bc316f61
ChenJR <827040662@qq.com>
2020-04-18 19:25:50 +0800

    237. 删除链表中的节点 1515. 面试题18. 删除链表的节点

--- deleteNode.php
+++ deleteNode.php
@@ -1,18 +1,34 @@
 <?php
 
 /**
+ *
+ * 237. 删除链表中的节点
+ * https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
  * 1515. 面试题18. 删除链表的节点
- * Class DeleteNodeSolution
  * https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/submissions/
+ *
+ * Class DeleteNodeSolution
+ *
  */
 class DeleteNodeSolution {
 
     /**
+     * 237
+     * @param ListNode $node
+     * @return void
+     */
+    function deleteNode_237($node) {
+        $node->val = $node->next->val;
+        $node->next = $node->next->next;
+    }
+
+    /**
+     * 1515
      * @param ListNode $head
      * @param Integer $val
      * @return ListNode
      */
-    function deleteNode($head, $val) {
+    function deleteNode_1515($head, $val) {
         if($head->val == $val){
             if($head->next == null){
                 return null;
@@ -20,7 +36,7 @@ class DeleteNodeSolution {
             $head->val = $head->next->val;
             $head->next = $head->next->next;
         }else{
-            $head->next = $this->deleteNode($head->next,$val);
+            $head->next = $this->deleteNode_1515($head->next,$val);
         }
         return $head;
     }
