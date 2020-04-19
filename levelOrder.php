e23f49e80feec1bdc6787f9e7708c207caed799e
ChenJR <827040662@qq.com>
2020-04-19 10:16:43 +0800

    102. 二叉树的层次遍历 429. N叉树的层序遍历 1536. 面试题32 - I. 从上到下打印二叉树 1537. 面试题32 - II. 从上到下打印二叉树 II 1562. 面试题32 - III. 从上到下打印二叉树 III

--- levelOrder.php
+++ levelOrder.php
@@ -9,6 +9,8 @@
  * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof/
  * 1537. 面试题32 - II. 从上到下打印二叉树 II
  * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+ * 1562. 面试题32 - III. 从上到下打印二叉树 III
+ * https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/
  *
  * Class LevelOrderSolution
  */
@@ -89,5 +91,51 @@ class LevelOrderSolution
                 $this->generateLevel($root->right, $rootLevel + 1, $result);
             }
         }
+    }
+
+    /**
+     * 1562 面试题32 - III. 从上到下打印二叉树 III
+     * BFS + 栈
+     * @param TreeNode $root
+     * @return Integer[][]
+     */
+    function levelOrder_1562($root)
+    {
+        if (!$root) return [];
+        $floor = new SplStack();
+        $floor->push($root);
+        $res = [[$root->val]];
+        $floor_num = 0;
+        while ($floor->count() > 0) {
+            $next_floor = new SplStack();
+            $floor_res = [];
+            $rev = $floor_num % 2 == 0;
+            while ($floor->count() > 0) {
+                $node = $floor->pop();
+                if ($rev) {
+                    if ($node->right) {
+                        $next_floor->push($node->right);
+                        $floor_res[] = $node->right->val;
+                    }
+                    if ($node->left) {
+                        $next_floor->push($node->left);
+                        $floor_res[] = $node->left->val;
+                    }
+                } else {
+                    if ($node->left) {
+                        $next_floor->push($node->left);
+                        $floor_res[] = $node->left->val;
+                    }
+                    if ($node->right) {
+                        $next_floor->push($node->right);
+                        $floor_res[] = $node->right->val;
+                    }
+                }
+            }
+            $floor = $next_floor;
+            !empty($floor_res) && $res[] = $floor_res;
+            $floor_num++;
+        }
+        return $res;
     }
 }
