35d80d01d54c1e27668fbbb2dc5665de04e9d3de
ChenJR <827040662@qq.com>
2020-05-19 07:32:33 +0800

    9. 回文数 125. 验证回文串

--- isPalindrome.php
+++ isPalindrome.php
@@ -2,8 +2,12 @@
 
 /**
  * 9. 回文数
- * Class IsPalindromeSolution
  * https://leetcode-cn.com/problems/palindrome-number/
+ *
+ * 125. 验证回文串
+ * https://leetcode-cn.com/problems/valid-palindrome/
+ *
+ * Class IsPalindromeSolution
  */
 class IsPalindromeSolution
 {
@@ -12,7 +16,7 @@ class IsPalindromeSolution
      * @param Integer $x
      * @return Boolean
      */
-    function isPalindrome($x)
+    function isPalindrome_9($x)
     {
         $strx = strval($x);
         $left = 0;
@@ -21,6 +25,22 @@ class IsPalindromeSolution
             if ($strx[$left++] !== $strx[$right--]) {
                 return false;
             }
+        }
+        return true;
+    }
+
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function isPalindrome_125($s)
+    {
+        $left = 0;
+        $right = strlen($s) - 1;
+        while ($left < $right) {
+            while ($left < $right && !ctype_alnum($s[$left])) $left++;
+            while ($left < $right && !ctype_alnum($s[$right])) $right--;
+            if (strtolower($s[$left++]) != strtolower($s[$right--])) return false;
         }
         return true;
     }
