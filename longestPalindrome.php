6177c768dc7ba5e9a4d6f53bcdb80ed946e8ba97
ChenJR <827040662@qq.com>
2020-05-21 09:09:33 +0800

    5. 最长回文子串 409. 最长回文串

--- longestPalindrome.php
+++ longestPalindrome.php
@@ -1,9 +1,13 @@
 <?php
 
 /**
+ *
+ * 5. 最长回文子串
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/
  * 409. 最长回文串
- * Class LongestPalindromeSolution
  * https://leetcode-cn.com/problems/longest-palindrome/
+ *
+ * Class LongestPalindromeSolution
  */
 class LongestPalindromeSolution
 {
@@ -12,7 +16,7 @@ class LongestPalindromeSolution
      * @param String $s
      * @return Integer
      */
-    function longestPalindrome($s)
+    function longestPalindrome_409($s)
     {
         $dictionary = [];
         foreach (str_split($s) as $str) {
@@ -30,4 +34,36 @@ class LongestPalindromeSolution
         }
         return $res + ($middle ? 1 : 0);
     }
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function longestPalindrome_5($s)
+    {
+        $n = strlen($s);
+        $dp = [];
+        $res = "";
+        for ($i = 1; $i <= $n; $i++) {
+            for ($j = 0; $j < $n; $j++) {
+                $m = $i + $j - 1;
+                if ($m >= $n) {
+                    break;
+                }
+                if ($i == 1) {
+                    $dp[$j][$m] = true;
+                } else if ($i == 2) {
+                    $dp[$j][$m] = ($s[$j] == $s[$m]);
+                } else {
+                    $dp[$j][$m] = ($dp[$j + 1][$m - 1] && $s[$j] == $s[$m]);
+                }
+                if ($dp[$j][$m] && $i > strlen($res)) {
+                    $res = substr($s, $j, $i);
+                }
+            }
+        }
+        return $res;
+    }
 }
+
+var_dump((new LongestPalindromeSolution())->longestPalindrome_5("babad"));
