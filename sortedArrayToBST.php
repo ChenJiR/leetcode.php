28d12cb7370eb5e96789146fe329252715faa960
ChenJR <827040662@qq.com>
2020-07-03 06:32:52 +0800

    108. 将有序数组转换为二叉搜索树 1616. 面试题 04.02. 最小高度树

--- sortedArrayToBST.php
+++ sortedArrayToBST.php
@@ -1,9 +1,14 @@
 <?php
 
+require "TreeNode.php";
+
 /**
- * 1413. 面试题 04.02. 最小高度树
- * Class SortedArrayToBSTSolution
+ * 1616. 面试题 04.02. 最小高度树
  * https://leetcode-cn.com/problems/minimum-height-tree-lcci/comments/
+ * 108. 将有序数组转换为二叉搜索树
+ * https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
+ *
+ * Class SortedArrayToBSTSolution
  */
 class SortedArrayToBSTSolution
 {
@@ -14,6 +19,13 @@ class SortedArrayToBSTSolution
      */
     function sortedArrayToBST($nums)
     {
-
+        $count = count($nums);
+        if ($count == 1) return new TreeNode($nums[0]);
+        if ($count == 0) return null;
+        $n = intval($count / 2);
+        $root = new TreeNode($nums[$n]);
+        $root->left = $this->sortedArrayToBST(array_slice($nums, 0, $n));
+        $root->right = $this->sortedArrayToBST(array_slice($nums, $n + 1));
+        return $root;
     }
 }
