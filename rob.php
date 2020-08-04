ca03a5ce86567ccceef01c2cda3fadd96e43006b
ChenJR <827040662@qq.com>
2020-08-05 06:56:44 +0800

    198. 打家劫舍 337. 打家劫舍 III

--- rob.php
+++ rob.php
@@ -2,17 +2,22 @@
 
 /**
  * 198. 打家劫舍
- * Class RobSolution
  * https://leetcode-cn.com/problems/house-robber/
+ *
+ * 337. 打家劫舍 III
+ * https://leetcode-cn.com/problems/house-robber-iii/
+ *
+ * Class RobSolution
  */
 class RobSolution
 {
 
     /**
+     * 198. 打家劫舍
      * @param Integer[] $nums
      * @return Integer
      */
-    function rob($nums)
+    function rob_1($nums)
     {
         if (empty($nums)) return 0;
         if (count($nums) < 2) return max($nums);
@@ -21,5 +26,25 @@ class RobSolution
             $dp[$i] = max($dp[$i - 2] + $nums[$i], $dp[$i - 1]);
         }
         return $dp[count($nums) - 1];
+    }
+
+    /**
+     * 337. 打家劫舍 III
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function rob_337($root)
+    {
+        return max($this->dfs($root));
+    }
+
+    private function dfs(TreeNode $node)
+    {
+        if (!$node) return [0, 0];
+        $l = $this->dfs($node->left);
+        $r = $this->dfs($node->right);
+        $selected = $node->val + $l[1] + $r[1];
+        $not_selected = max($l[0], $l[1]) + max($r[0], $r[1]);
+        return [$selected, $not_selected];
     }
 }
